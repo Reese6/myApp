8f8bfe88058021ab5c5d0bcd88980833e7acf6fd
Reese6 <kurnosov1998@list.ru>
2020-07-07 20:07:46 +0300

    outh: work on InputGroup, added uuid

--- client/js/react/globalComponents/InputGroup/InputGroup.js
+++ client/js/react/globalComponents/InputGroup/InputGroup.js
@@ -1,29 +1,65 @@
-import React from 'react';
+import React, { Fragment } from 'react';
 import * as TYPES from 'prop-types';
+import { v4 } from 'uuid';
 
 function InputGroup({
-  
+  label,
+  id,
+  value,
+  hasError,
+  isRequired,
+  onChange,
+  onFocus,
+  onBlur,
 }) {
+  const className = [
+    'form-froup',
+    hasError || 'error',
+  ].join(' ');
+
   return (
-    <div className="form-group">
-      <label></label>
-      <input />
+    <div className={classNameForGroup}>
+      {label.length > 0 && (
+        <label for={id}>
+          <span>{label}</span>
+          {isRequired && <span className="form-group__star-reqired"> *</span>}
+        </label>
+      )}
+      <input
+        id={id}
+        name={name}
+        value={value}
+        onChange={onChange}
+        onFocus={onFocus}
+        onBlur={onBlur}
+        autoCompleteInput="off"
+      />
     </div>
   );
 }
 
 InputGroup.propTypes = {
+  name: TYPES.string,
+  id: TYPES.string,
   label: TYPES.string.isRequired,
   value: TYPES.string.isRequired,
   hasError: TYPES.bool,
   isRequired: TYPES.bool,
+  disabled: TYPES.bool,
   onChange: TYPES.func.isRequired,
   onFocus: TYPES.func.isRequired,
   onBlur: TYPES.func.isRequired,
 };
 
 InputGroup.defaultProps = {
-
+  label: '',
+  name: '',
+  id: `input-group-${v4()}`,
+  hasError: false,
+  isRequired: false,
+  disabled: false,
+  onFocus: () => {},
+  onBlur: () => {},
 };
 
 export default InputGroup;
