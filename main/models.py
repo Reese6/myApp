d23c3e926fccc93abffb8ee21d6545b1e8e1e76b
Reese6 <kurnosov1998@list.ru>
2020-08-14 16:37:11 +0300

    added base models

--- main/models.py
+++ main/models.py
@@ -1,3 +1,49 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class Project(models.Model):
+    name = models.CharField(max_length=100)
+    description = models.TextField()
+    admin = models.ForeignKey(User, on_delete=models.CASCADE)
+    users = models.ManyToManyField(User)
+    create_date = models.DateField()
+
+    def __str__(self):
+        return self.name
+
+
+class Board(models.Model):
+    name = models.CharField(max_length=100)
+    project = models.ForeignKey(Project, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.name
+
+
+class Column(models.Model):
+    name = models.CharField(max_length=100)
+    index = models.IntegerField()
+    board = models.ForeignKey(Board, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.name
+
+
+class Task(models.Model):
+    name = models.CharField(max_length=150)
+    description = models.TextField()
+    users = models.ManyToManyField(User)
+    column = models.ForeignKey(Column, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.name
+
+
+class CommentTask(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    task = models.ForeignKey(Task, on_delete=models.CASCADE)
+    comment = models.CharField(max_length=150)
+
+    def __str__(self):
+        return self.comment
