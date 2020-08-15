2d448a9d0f2f994a7a6a7bef93946d0e29af61e8
Reese6 <kurnosov1998@list.ru>
2020-08-15 11:02:07 +0300

    master: addeed models in admin site

--- main/admin.py
+++ main/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
 
 # Register your models here.
+from .models import Project, Board, Column, Task, CommentTask
+
+
+admin.site.register(Project)
+admin.site.register(Board)
+admin.site.register(Column)
+admin.site.register(Task)
+admin.site.register(CommentTask)
--- main/models.py
+++ main/models.py
@@ -5,8 +5,8 @@ from django.contrib.auth.models import User
 class Project(models.Model):
     name = models.CharField(max_length=100)
     description = models.TextField()
-    admin = models.ForeignKey(User, on_delete=models.CASCADE)
-    users = models.ManyToManyField(User)
+    admin = models.ForeignKey(User, on_delete=models.CASCADE, related_name='admin')
+    users = models.ManyToManyField(User, related_name='users')
     create_date = models.DateField()
 
     def __str__(self):
