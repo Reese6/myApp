14aa9224730ba79467eb2c76bad29d6490dfb25b
Reese6 <kurnosov1998@list.ru>
2020-08-16 14:35:30 +0300

    master: work on projects page

--- client/js/react/views/Projects/containers/Container/Container.js
+++ client/js/react/views/Projects/containers/Container/Container.js
@@ -1,10 +1,18 @@
 import React from 'react';
 // import * as TYPES from 'prop-types';
 
-import { TitlePage } from '~/globalComponents';
+import { TitlePage, Button } from '~/globalComponents';
+import Projects from '../Projects';
 
 function Container() {
-  return <TitlePage title="Проекты" />;
+  return (
+    <div className="projects">
+      <TitlePage title="Проекты">
+        <Button primary title="Новый проект" />
+      </TitlePage>
+      <Projects />
+    </div>
+  );
 }
 
 Container.propTypes = {};
