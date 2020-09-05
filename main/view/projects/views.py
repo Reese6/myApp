bc30a3f22601195f553ebd508aaaf6161e1b3066
Reese6 <kurnosov1998@list.ru>
2020-09-05 17:15:01 +0300

    master: modify style code

--- main/view/projects/views.py
+++ main/view/projects/views.py
@@ -1,8 +1,7 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render
 from django.http import JsonResponse
 from django.http import Http404
 from main.models import Project, Task, Column
-# from django.forms.models import model_to_dict
 
 
 def index(request):
@@ -20,17 +19,17 @@ def show(request, project_id):
         raise Http404("Страница не найдена :(")
 
 
+# def newProject(request):
+#     if request.method == 'POST':
+
+
 def get_projets(request):
-    if request.user.is_authenticated:
-        data = Project.objects.filter(users=request.user).values()
-        return JsonResponse({'data': list(data), 'status': True}, safe=False)
-    else:
-        return JsonResponse({'status': False})
+    data = Project.objects.filter(users=request.user).values()
+    return JsonResponse({'data': list(data), 'status': True}, safe=False)
 
 
 def get_show_project(request, project_id):
-    if request.user.is_authenticated:
-        columns = Column.objects.filter(project__id=project_id)
-        project = Project.objects.filter(pk=project_id).values()
-        tasks = Task.objects.filter(column_id__in=columns).values()
-        return JsonResponse({'project': list(project), 'columns': list(columns.values()), 'tasks': list(tasks), 'status': True}, safe=False)
+    columns = Column.objects.filter(project__id=project_id)
+    project = Project.objects.filter(pk=project_id).values()
+    tasks = Task.objects.filter(column_id__in=columns).values()
+    return JsonResponse({'project': list(project), 'columns': list(columns.values()), 'tasks': list(tasks), 'status': True}, safe=False)
